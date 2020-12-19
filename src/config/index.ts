157ec2b96e4d58054f00e2429b8d4827794b3748
Alex Vayn <kingred@gmail.com>
2020-12-19 18:07:56 -0500

    version 0: basic running project

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,11 +9,17 @@ export default {
   port: parseInt(process.env.PORT, 10),
   archiveRootDir: process.env.ARCHIVE_ROOT,
   trashDir: process.env.TRASH_DIR,
-  usedMetatdataDir: process.env.USED_METADATA_DIR,
+  metaDataArchivePath: process.env.META_ARCHIVE_DIR,
+  testImagePath: process.env.TEST_IMAGE_PATH,
   logs: {
     level: process.env.LOG_LEVEL || 'silly'
   },
   api: {
     prefix: '/api'
-  }
+  },
+  healthCheckEndpoint: getHealthCheckEndpoint()
 };
+
+function getHealthCheckEndpoint() {
+  return 'http://localhost:' + parseInt(process.env.PORT, 10) + '/api/archive/healthcheck';
+}
